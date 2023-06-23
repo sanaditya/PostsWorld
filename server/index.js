387ff02c55b2865cb0cdbe1b2f6db309fd699c53
Aditya <adityasantra.11nov@gmail.com>
2023-06-24 01:49:12 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -1,103 +1,25 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
-import {register} from "./controllers/auth.js"
-import {createPost} from "./controllers/posts.js"
-import { verifyToken } from "./middleware/auth.js";
-// import User from "./models/User.js";
-// import Post from "./models/Post.js";
-// import { users, posts } from "./data/index.js";
-
-//CONFIGURATION
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
-
-//FILE STORAGE
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage });
-
-/* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
-
-//ROUTES 
-
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
-
-//MONGOOSE SETUP
-
-const PORT = process.env.PORT || 6001;
-const uri="mongodb+srv://aditya1234:ZLTkZpFbCtPrMc59@cluster1.cjlmbkb.mongodb.net/?retryWrites=true&w=majority"
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-    // User.insertMany(users);
-    // Post.insertMany(posts);
-
-  })
-  .catch((error) => console.log(`${error} did not connect`));
-
-
-
- 
-// import fs from "fs"
 // import express from "express";
 // import bodyParser from "body-parser";
 // import mongoose from "mongoose";
 // import cors from "cors";
-// import dotenv from "dotenv";
-// import multer, { memoryStorage } from "multer";
+// import dotenv from "dotenv/config";
+// import multer from "multer";
 // import helmet from "helmet";
 // import morgan from "morgan";
 // import path from "path";
 // import { fileURLToPath } from "url";
-// import authRoutes from "./routes/auth.js";
-// import userRoutes from "./routes/users.js";
-// import postRoutes from "./routes/posts.js";
-// import { register } from "./controllers/auth.js";
-// import { createPost } from "./controllers/posts.js";
+// import authRoutes from "./routes/auth.js"
+// import userRoutes from "./routes/users.js"
+// import postRoutes from "./routes/posts.js"
+// import { register } from "./controllers/auth.js"
+// import { createPost } from "./controllers/posts.js"
 // import { verifyToken } from "./middleware/auth.js";
 // import User from "./models/User.js";
 // import Post from "./models/Post.js";
 // import { users, posts } from "./data/index.js";
 
-// /* CONFIGURATIONS */
+
+// //CONFIGURATION
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 // dotenv.config();
@@ -112,44 +34,32 @@ mongoose
 // app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 
-// /* FILE STORAGE */
-// // const storage = multer.diskStorage({
-// //   destination: function (req, file, cb) {
-// //     cb(null, "public/assets");
-// //   },
-// //   filename: function (req, file, cb) {
-// //     cb(null, file.originalname);
-// //   },
-// // });
-// // const upload = multer({ storage });
+// //FILE STORAGE
 
-// const uploader= multer({
-//   storage: multer.memoryStorage()
-// })
-
-// app.post("/auth/register", uploader.single("file"), async (req,res)=>{
-//   const file=req.file;
-
-//   fs.writeFileSync(__dirname + "/public/assets/" + file.originalname, file.buffer);
-
-//   res.status(200).json({
-//     status: "ok",
-//   })
-// }, register)
-// app.post("/posts", verifyToken, uploader.single("file"), createPost);
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/assets");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage });
 
 // /* ROUTES WITH FILES */
-// // app.post("/auth/register", upload.single("picture"), register);
-// // app.post("/posts", verifyToken, upload.single("file"), createPost);
+// app.post("/auth/register", upload.single("picture"), register);
+// app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-// /* ROUTES */
+// //ROUTES 
+
 // app.use("/auth", authRoutes);
 // app.use("/users", userRoutes);
 // app.use("/posts", postRoutes);
 
-// /* MONGOOSE SETUP */
-// const PORT = process.env.PORT || 6001;
-// const uri="mongodb+srv://aditya1234:ZLTkZpFbCtPrMc59@cluster1.cjlmbkb.mongodb.net/?retryWrites=true&w=majority"
+// //MONGOOSE SETUP
+
+// const uri = "mongodb+srv://adityasocial:o8msUpEe82uxEjqt@cluster0.yxwuefr.mongodb.net/?retryWrites=true&w=majority"
+// const PORT = 3001 || 6001;
 // mongoose
 //   .connect(uri, {
 //     useNewUrlParser: true,
@@ -157,12 +67,81 @@ mongoose
 //   })
 //   .then(() => {
 //     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-//     // User.insertMany(users);
-//     // Post.insertMany(posts);
+//     User.insertMany(users);
+//     Post.insertMany(posts);
 
 //   })
 //   .catch((error) => console.log(`${error} did not connect`));
 
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
-  
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+/* MONGOOSE SETUP */
+const uri = "mongodb+srv://adityasantra11nov:USfzIZwoLnkhkxJh@cluster0.3sxq70p.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 6001;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
